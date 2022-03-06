5d5c8a8737a2db37f8c5bacebeedb44b2491fbee
Fusion1013 <william.reutmer@gmail.com>
2022-03-06 17:31:21 +0100

    Add automatic format for location objects

--- src/main/java/se/fusion1013/plugin/cobaltcore/util/StringPlaceholders.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/util/StringPlaceholders.java
@@ -1,8 +1,12 @@
 package se.fusion1013.plugin.cobaltcore.util;
 
+import org.bukkit.Location;
+
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,8 +14,19 @@ public class StringPlaceholders {
 
     private Map<String, String> placeholders;
 
+    private static final Map<String, String> variableDefaultStyles = new LinkedHashMap<String, String>(){
+        {
+            put("location", "&b%x%&7, &b%y%&7, &b%z%&7");
+            put("time", "&b%days%&7 days, &b%hours%&7 hours, &b%minutes%&7 minutes, &b%seconds%&7 seconds, &b%milliseconds%&7 milliseconds");
+            put("timestamp", "&b%value%&7 %format%");
+        }};
+
     public StringPlaceholders(){
         this.placeholders = new HashMap<>();
+    }
+
+    public void addPlaceholder(String placeholder, Map<TimeUnit, Long> time) {
+        this.placeholders.put(placeholder, objectToString(time));
     }
 
     public void addPlaceholder(String placeholder, Object value){
@@ -45,8 +60,45 @@ public class StringPlaceholders {
         return builder(placeholder, value).build();
     }
 
+    private static String objectToString(Map<TimeUnit, Long> diff) { // TODO: Redo this method
+        if (diff == null) return "null";
+        String string = "";
+        if (diff.get(TimeUnit.DAYS) != 0) string += StringPlaceholders.builder()
+                .addPlaceholder("value", diff.get(TimeUnit.DAYS))
+                .addPlaceholder("format", "days").build()
+                .apply(variableDefaultStyles.get("timestamp")) + ", ";
+        if (diff.get(TimeUnit.HOURS) != 0) string += StringPlaceholders.builder()
+                .addPlaceholder("value", diff.get(TimeUnit.HOURS))
+                .addPlaceholder("format", "hours").build()
+                .apply(variableDefaultStyles.get("timestamp")) + ", ";
+        if (diff.get(TimeUnit.MINUTES) != 0) string += StringPlaceholders.builder()
+                .addPlaceholder("value", diff.get(TimeUnit.MINUTES))
+                .addPlaceholder("format", "minutes").build()
+                .apply(variableDefaultStyles.get("timestamp")) + ", ";
+        if (diff.get(TimeUnit.SECONDS) != 0) string += StringPlaceholders.builder()
+                .addPlaceholder("value", diff.get(TimeUnit.SECONDS))
+                .addPlaceholder("format", "seconds").build()
+                .apply(variableDefaultStyles.get("timestamp")) + ", ";
+        if (diff.get(TimeUnit.MILLISECONDS) != 0) string += StringPlaceholders.builder()
+                .addPlaceholder("value", diff.get(TimeUnit.MILLISECONDS))
+                .addPlaceholder("format", "milliseconds").build()
+                .apply(variableDefaultStyles.get("timestamp")) + ", ";
+
+        return string.substring(0, Math.max(0, string.length() - 2));
+    }
+
     private static String objectToString(Object object){
-        return object != null ? object.toString() : "null";
+        if (object == null) return "null";
+        if (object instanceof Location location) {
+            StringPlaceholders placeholders = StringPlaceholders.builder()
+                    .addPlaceholder("x", Math.round(location.getX() * 10) / 10.0)
+                    .addPlaceholder("y", Math.round(location.getY() * 10) / 10.0)
+                    .addPlaceholder("z", Math.round(location.getZ() * 10) / 10.0)
+                    .build();
+            return placeholders.apply(variableDefaultStyles.get("location"));
+        }
+
+        return object.toString();
     }
 
     public static class Builder{
@@ -64,6 +116,11 @@ public class StringPlaceholders {
 
         public Builder addPlaceholder(String placeholder, Object value){
             this.stringPlaceholders.addPlaceholder(placeholder, objectToString(value));
+            return this;
+        }
+
+        public Builder addPlaceholder(String placeholder, Map<TimeUnit, Long> time) {
+            this.stringPlaceholders.addPlaceholder(placeholder, time);
             return this;
         }
 
