acbad57e7cbb75f32c03dfc4f8689c64c9f01867
Fusion1013 <william.reutmer@gmail.com>
2022-03-06 17:27:49 +0100

    Create AbilityModule.java

---
+++ src/main/java/se/fusion1013/plugin/cobaltcore/entity/modules/ability/AbilityModule.java
@@ -0,0 +1,8 @@
+package se.fusion1013.plugin.cobaltcore.entity.modules.ability;
+
+import se.fusion1013.plugin.cobaltcore.entity.modules.EntityModule;
+
+/**
+ * Extend this class to create a custom ability for an entity.
+ */
+public abstract class AbilityModule extends EntityModule implements IAbilityModule { }
