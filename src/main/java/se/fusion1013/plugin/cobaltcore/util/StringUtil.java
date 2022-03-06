ecac616ab545dcaee7086c1739ce665006a048b7
Fusion1013 <william.reutmer@gmail.com>
2022-03-06 17:31:56 +0100

    Add equalsIgnoreCase method for multiple inputs

--- src/main/java/se/fusion1013/plugin/cobaltcore/util/StringUtil.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/util/StringUtil.java
@@ -1,7 +1,30 @@
 package se.fusion1013.plugin.cobaltcore.util;
 
+/**
+ * Contains various utility methods for working with strings.
+ */
 public class StringUtil {
+
+    /**
+     * Checks whether the provided string is a word or not.
+     *
+     * @param toCheck the string to check.
+     * @return true if the string is a word.
+     */
     public static boolean isWord(String toCheck){
         return toCheck.matches("\\w+$");
+    }
+
+    /**
+     * Checks if the value is equal to any of the compares.
+     * @param toParse value to check.
+     * @param compare values to compare with.
+     * @return boolean value.
+     */
+    public static boolean equalsIgnoreCase(String toParse, String... compare) {
+        for (String s : compare) {
+            if (s.equalsIgnoreCase(toParse)) return true;
+        }
+        return false;
     }
 }
