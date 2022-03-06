92a72f65076d63d447ef85bea0fe88c2a3eea2da
Fusion1013 <william.reutmer@gmail.com>
2022-03-06 17:32:04 +0100

    Create VersionUtil.java

---
+++ src/main/java/se/fusion1013/plugin/cobaltcore/util/VersionUtil.java
@@ -0,0 +1,29 @@
+package se.fusion1013.plugin.cobaltcore.util;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
+import se.fusion1013.plugin.cobaltcore.CobaltPlugin;
+import se.fusion1013.plugin.cobaltcore.manager.LocaleManager;
+
+public class VersionUtil {
+
+    /**
+     * Prints the plugin version to the player.
+     *
+     * @param plugin the plugin to get the version from.
+     * @param player the player to send the version to.
+     */
+    public static void printVersion(CobaltPlugin plugin, Player player) {
+        PluginDescriptionFile desc = plugin.getDescription();
+        LocaleManager localeManager = LocaleManager.getInstance();
+        StringPlaceholders placeholders = StringPlaceholders.builder()
+                .addPlaceholder("plugin_name", desc.getName())
+                .addPlaceholder("version", desc.getVersion())
+                .addPlaceholder("github_issues_link", "") // TODO: ADD GITHUB
+                .build();
+
+        localeManager.sendMessage(plugin, player, "version.version", placeholders);
+        localeManager.sendMessage(plugin, player, "version.author", placeholders);
+        // localeManager.sendMessage(player, new Message("version.github_issues"), placeholders);
+    }
+}
