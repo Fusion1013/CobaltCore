1b852c91d9c4efb5c2f30310c153f7da0403ec84
Fusion1013 <william.reutmer@gmail.com>
2022-03-06 17:30:20 +0100

    Create Constants.java

---
+++ src/main/java/se/fusion1013/plugin/cobaltcore/util/Constants.java
@@ -0,0 +1,13 @@
+package se.fusion1013.plugin.cobaltcore.util;
+
+import org.bukkit.NamespacedKey;
+import se.fusion1013.plugin.cobaltcore.CobaltCore;
+
+/**
+ * Stores <code>NamespacedKey</code>'s for use elsewhere in the plugin.
+ */
+public class Constants {
+
+    public static final NamespacedKey TEMPORARY_BLACKLISTED = new NamespacedKey(CobaltCore.getInstance(), "temporary_blacklisted");
+
+}
