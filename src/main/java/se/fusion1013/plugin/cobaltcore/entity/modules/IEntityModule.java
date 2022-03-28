28647ebf9402ad15526d6cfa2de62195635db676
Fusion1013 <william.reutmer@gmail.com>
2022-03-28 22:30:18 +0200

    Create IEntityModule.java

---
+++ src/main/java/se/fusion1013/plugin/cobaltcore/entity/modules/IEntityModule.java
@@ -0,0 +1,9 @@
+package se.fusion1013.plugin.cobaltcore.entity.modules;
+
+import se.fusion1013.plugin.cobaltcore.entity.CustomEntity;
+
+public interface IEntityModule {
+
+    void execute(CustomEntity customEntity);
+
+}
