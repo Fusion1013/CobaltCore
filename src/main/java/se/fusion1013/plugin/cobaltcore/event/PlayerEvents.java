b2784450b697cea3d3d5f4830d4562c8f688466d
Fusion1013 <william.reutmer@gmail.com>
2022-03-28 22:30:47 +0200

    Send all CobaltPlugin versions on player join

--- src/main/java/se/fusion1013/plugin/cobaltcore/event/PlayerEvents.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/event/PlayerEvents.java
@@ -5,6 +5,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import se.fusion1013.plugin.cobaltcore.CobaltCore;
+import se.fusion1013.plugin.cobaltcore.CobaltPlugin;
 import se.fusion1013.plugin.cobaltcore.manager.LocaleManager;
 import se.fusion1013.plugin.cobaltcore.util.StringPlaceholders;
 
@@ -17,10 +18,14 @@ public class PlayerEvents implements Listener {
         Player p = event.getPlayer();
 
         LocaleManager localeManager = LocaleManager.getInstance();
-        StringPlaceholders placeholders = StringPlaceholders.builder()
-                        .addPlaceholder("version", CobaltCore.getInstance().getDescription().getVersion())
-                                .build();
-        localeManager.sendMessage(p, "cobalt.player.join", placeholders);
+
+        for (CobaltPlugin plugin : CobaltCore.getRegisteredCobaltPlugins()) {
+            StringPlaceholders placeholders = StringPlaceholders.builder()
+                    .addPlaceholder("plugin", plugin.getName())
+                    .addPlaceholder("version", plugin.getDescription().getVersion())
+                    .build();
+            localeManager.sendMessage(plugin, p, "cobalt.player.join", placeholders);
+        }
     }
 
 }
