d67a8c055bd502f8dad26191e57b5ed0ac0f37e9
Fusion1013 <william.reutmer@gmail.com>
2022-03-28 22:26:48 +0200

    Rework to use new entity system

--- src/main/java/se/fusion1013/plugin/cobaltcore/commands/CobaltSummonCommand.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/commands/CobaltSummonCommand.java
@@ -1,12 +1,13 @@
 package se.fusion1013.plugin.cobaltcore.commands;
 
 import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.IntegerArgument;
 import dev.jorel.commandapi.arguments.LocationArgument;
 import dev.jorel.commandapi.arguments.StringArgument;
 import org.bukkit.Location;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import se.fusion1013.plugin.cobaltcore.CobaltCore;
-import se.fusion1013.plugin.cobaltcore.entity.CustomEntity;
 import se.fusion1013.plugin.cobaltcore.entity.CustomEntityManager;
 import se.fusion1013.plugin.cobaltcore.manager.LocaleManager;
 import se.fusion1013.plugin.cobaltcore.util.StringPlaceholders;
@@ -15,48 +16,101 @@ public class CobaltSummonCommand {
 
     // ----- REGISTER -----
 
+    /**
+     * Registers the csummon command.
+     */
     public static void register() {
+        createSummonManyAtLocationCommand().register();
+        createSummonManyCommand().register();
+        createSummonOneAtLocationCommand().register();
+        createSummonOneCommand().register();
+    }
 
-        new CommandAPICommand("csummon")
-                .withPermission("cobalt.core.command.csummon")
-                .withArguments(new StringArgument("entity").replaceSuggestions(info -> CustomEntityManager.getInternalEntityNames()))
-                .executesPlayer(((sender, args) -> {
-                    String name = (String)args[0];
-                    Location location = sender.getLocation();
+    // ----- SUMMON MANY AT LOCATION COMMAND -----
 
-                    boolean summoned = CustomEntityManager.forceSummonEntity(name, location);
-                    StringPlaceholders placeholders = StringPlaceholders.builder()
-                            .addPlaceholder("name", name)
-                            .addPlaceholder("location", location)
-                            .build();
-
-                    if (summoned) LocaleManager.getInstance().sendMessage(CobaltCore.getInstance(), sender, "commands.csummon.summoned_entity", placeholders);
-                    else LocaleManager.getInstance().sendMessage(CobaltCore.getInstance(), sender, "commands.csummon.could_not_summon", placeholders);
-                })).register();
-
-        new CommandAPICommand("csummon")
+    private static CommandAPICommand createSummonManyAtLocationCommand() {
+        return new CommandAPICommand("csummon")
                 .withPermission("cobalt.core.command.csummon")
                 .withArguments(new StringArgument("entity").replaceSuggestions(info -> CustomEntityManager.getInternalEntityNames()))
                 .withArguments(new LocationArgument("location"))
+                .withArguments(new IntegerArgument("count"))
                 .executes(((sender, args) -> {
-                    String name = (String) args[0];
-                    Location location = (Location) args[1];
-
-                    boolean summoned = CustomEntityManager.forceSummonEntity(name, location);
-
-                    if (sender instanceof Player player) {
-                        StringPlaceholders placeholders = StringPlaceholders.builder()
-                                .addPlaceholder("name", name)
-                                .addPlaceholder("location", location)
-                                .build();
-
-                        if (summoned)
-                            LocaleManager.getInstance().sendMessage(CobaltCore.getInstance(), player, "commands.csummon.summoned_entity", placeholders);
-                        else
-                            LocaleManager.getInstance().sendMessage(CobaltCore.getInstance(), player, "commands.csummon.could_not_summon", placeholders);
-                    }
-                })).register();
-
+                    summonManyAtLocation(sender, (String)args[0], (Location)args[1], (int)args[2]);
+                }));
     }
 
+    // ----- SUMMON MANY COMMAND -----
+
+    /**
+     * Creates the summon command that summons many entities at the players position.
+     *
+     * @return the command.
+     */
+    private static CommandAPICommand createSummonManyCommand() {
+        return new CommandAPICommand("csummon")
+                .withPermission("cobalt.core.command.csummon")
+                .withArguments(new StringArgument("entity").replaceSuggestions(info -> CustomEntityManager.getInternalEntityNames()))
+                .withArguments(new IntegerArgument("count"))
+                .executesPlayer(((sender, args) -> {
+                    summonManyAtLocation(sender, (String)args[0], sender.getLocation(), (int)args[1]);
+                }));
+    }
+
+    // ----- SUMMON ONE AT LOCATION COMMAND -----
+
+    /**
+     * Creates the summon command that summons one entity at the specified location.
+     *
+     * @return the command.
+     */
+    private static CommandAPICommand createSummonOneAtLocationCommand() {
+        return new CommandAPICommand("csummon")
+                .withPermission("cobalt.core.command.csummon")
+                .withArguments(new StringArgument("entity").replaceSuggestions(info -> CustomEntityManager.getInternalEntityNames()))
+                .withArguments(new LocationArgument("location"))
+                .executes((sender, args) -> {
+                    summonManyAtLocation(sender, (String)args[0], (Location)args[1], 1);
+                });
+    }
+
+    // ----- SUMMON ONE COMMAND -----
+
+    /**
+     * Creates the summon command that summons one entity at the executors' location.
+     *
+     * @return the command.
+     */
+    private static CommandAPICommand createSummonOneCommand() {
+        return new CommandAPICommand("csummon")
+                .withPermission("cobalt.core.command.csummon")
+                .withArguments(new StringArgument("entity").replaceSuggestions(info -> CustomEntityManager.getInternalEntityNames()))
+                .executesPlayer(((sender, args) -> {
+                    summonManyAtLocation(sender, (String)args[0], sender.getLocation(), 1);
+                }));
+    }
+
+    // ----- LOGIC -----
+
+    /**
+     * Summons the specified number of <code>CustomEntity</code>'s at the <code>Location</code>.
+     *
+     * @param sender the sender that is summoning the entities.
+     * @param entityName the name of the <code>CustomEntity</code>.
+     * @param location the <code>Location</code> to summon the <code>CustomEntity</code> at.
+     * @param count the number of <code>CustomEntity</code>'s to summon.
+     */
+    private static void summonManyAtLocation(CommandSender sender, String entityName, Location location, int count) {
+
+        for (int i = 0; i < count; i++) CustomEntityManager.forceSummonEntity(entityName, location);
+
+        StringPlaceholders placeholders = StringPlaceholders.builder()
+                .addPlaceholder("name", entityName)
+                .addPlaceholder("location", location)
+                .addPlaceholder("count", count)
+                .build();
+
+        if (sender instanceof Player player) {
+            LocaleManager.getInstance().sendMessage(CobaltCore.getInstance(), player, "commands.csummon.summoned_entity", placeholders);
+        }
+    }
 }
