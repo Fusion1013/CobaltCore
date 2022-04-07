c91f654b0572dcc52d815ea82bab33ed10f182cb
Fusion1013 <william.reutmer@gmail.com>
2022-04-07 19:26:31 +0200

    Update ParticleGroup.java

--- src/main/java/se/fusion1013/plugin/cobaltcore/particle/ParticleGroup.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/particle/ParticleGroup.java
@@ -3,9 +3,11 @@ package se.fusion1013.plugin.cobaltcore.particle;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 import se.fusion1013.plugin.cobaltcore.particle.styles.IParticleStyle;
 import se.fusion1013.plugin.cobaltcore.particle.styles.ParticleStyle;
 import se.fusion1013.plugin.cobaltcore.util.ParticleContainer;
+import se.fusion1013.plugin.cobaltcore.util.VectorUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +22,7 @@ public class ParticleGroup implements Cloneable {
 
     UUID uuid;
     String name;
-    private final List<ParticleStyle> particleStyleList;
+    private final List<ParticleStyleHolder> particleStyleList;
 
     // ----- CONSTRUCTORS -----
 
@@ -55,10 +57,94 @@ public class ParticleGroup implements Cloneable {
      */
     public ParticleGroup(ParticleGroup target) {
         this.uuid = target.uuid;
+        this.name = target.name;
         this.particleStyleList = target.particleStyleList;
     }
 
+    // ----- PARTICLE STYLE INFO -----
+
+    /**
+     * Gets an array of particle style names contained within this group.
+     *
+     * @return an array of particle style names.
+     */
+    public String[] getParticleStyleNames() {
+        String[] names = new String[particleStyleList.size()];
+        for (int i = 0; i < names.length; i++) {
+            names[i] = particleStyleList.get(i).style.getName();
+        }
+        return names;
+    }
+
+    // ----- PARTICLE STYLE EDITING -----
+
+    public void setStyleOffset(String styleName, Vector offset) {
+        ParticleStyleHolder holder = getHolder(styleName);
+        if (holder == null) return;
+        holder.offset = offset;
+    }
+
+    public void setStyleRotation(String styleName, Vector rotation, Vector rotationSpeed) {
+        ParticleStyleHolder holder = getHolder(styleName);
+        if (holder == null) return;
+        holder.rotation = rotation;
+        holder.rotationSpeed = rotationSpeed;
+    }
+
+    private ParticleStyleHolder getHolder(String styleName) {
+        for (ParticleStyleHolder holder : particleStyleList) if (holder.style.getName().equalsIgnoreCase(styleName)) return holder;
+        return null;
+    }
+
     // ----- PARTICLE DISPLAY -----
+
+    /**
+     * Displays all particles in the <code>ParticleGroup</code> in relation to one <code>Location</code>, to the given players.
+     * @param location the location to center the <code>ParticleGroup</code> on.
+     * @param players the players to display the <code>ParticleGroup</code> to.
+     */
+    public void display(Location location, Player... players) {
+        if (location == null) return;
+
+        // Display the particle for all online players
+        for (Player p : players){
+            for (ParticleStyleHolder psh : particleStyleList){
+                ParticleContainer[] particles = psh.getParticles(location);
+                Object extra = psh.style.getExtra();
+
+                // Display all particles in the style
+                for (ParticleContainer particle : particles){
+                    if (extra != null) p.spawnParticle(psh.style.getParticle(), particle.getLocation(), particle.getCount(), particle.getxOff(), particle.getyOff(), particle.getzOff(), particle.getSpeed(), extra);
+                    else p.spawnParticle(psh.style.getParticle(), particle.getLocation(), particle.getCount(), particle.getxOff(), particle.getyOff(), particle.getzOff(), particle.getSpeed());
+                }
+            }
+        }
+    }
+
+    /**
+     * Displays all particles in the <code>ParticleGroup</code> in relation to two <code>Location</code>'s, to the given players.
+     *
+     * @param location1 the first location.
+     * @param location2 the second location.
+     * @param players the players to display the <code>ParticleGroup</code> to.
+     */
+    public void display(Location location1, Location location2, Player... players) {
+        if (location1 == null || location2 == null) return;
+
+        // Display the particle for all online players
+        for (Player p : players){
+            for (ParticleStyleHolder psh : particleStyleList) {
+                ParticleContainer[] particles = psh.getParticles(location1, location2);
+                Object extra = psh.style.getExtra();
+
+                // Display all particles in the style
+                for (ParticleContainer particle : particles){
+                    if (extra != null) p.spawnParticle(psh.style.getParticle(), particle.getLocation(), particle.getCount(), particle.getxOff(), particle.getyOff(), particle.getzOff(), particle.getSpeed(), extra);
+                    else p.spawnParticle(psh.style.getParticle(), particle.getLocation(), particle.getCount(), particle.getxOff(), particle.getyOff(), particle.getzOff(), particle.getSpeed());
+                }
+            }
+        }
+    }
 
     /**
      * Displays all particles in the <code>ParticleGroup</code> in relation to one <code>Location</code>.
@@ -66,21 +152,7 @@ public class ParticleGroup implements Cloneable {
      * @param location the location to center the <code>ParticleGroup</code> on.
      */
     public void display(Location location) {
-        if (location == null) return;
-
-        // Display the particle for all online players
-        for (Player p : Bukkit.getOnlinePlayers()){
-            for (IParticleStyle ps : particleStyleList){
-                ParticleContainer[] particles = ps.getParticles(location);
-                Object extra = ps.getExtra();
-
-                // Display all particles in the style
-                for (ParticleContainer particle : particles){
-                    if (extra != null) p.spawnParticle(ps.getParticle(), particle.getLocation(), particle.getCount(), particle.getxOff(), particle.getyOff(), particle.getzOff(), particle.getSpeed(), extra);
-                    else p.spawnParticle(ps.getParticle(), particle.getLocation(), particle.getCount(), particle.getxOff(), particle.getyOff(), particle.getzOff(), particle.getSpeed());
-                }
-            }
-        }
+        display(location, Bukkit.getOnlinePlayers().toArray(new Player[0]));
     }
 
     /**
@@ -90,24 +162,17 @@ public class ParticleGroup implements Cloneable {
      * @param location2 the second location.
      */
     public void display(Location location1, Location location2) {
-        if (location1 == null || location2 == null) return;
-
-        // Display the particle for all online players
-        for (Player p : Bukkit.getOnlinePlayers()){
-            for (IParticleStyle ps : particleStyleList){
-                ParticleContainer[] particles = ps.getParticles(location1, location2);
-                Object extra = ps.getExtra();
-
-                // Display all particles in the style
-                for (ParticleContainer particle : particles){
-                    if (extra != null) p.spawnParticle(ps.getParticle(), particle.getLocation(), particle.getCount(), particle.getxOff(), particle.getyOff(), particle.getzOff(), particle.getSpeed(), extra);
-                    else p.spawnParticle(ps.getParticle(), particle.getLocation(), particle.getCount(), particle.getxOff(), particle.getyOff(), particle.getzOff(), particle.getSpeed());
-                }
-            }
-        }
+        display(location1, location2, Bukkit.getOnlinePlayers().toArray(new Player[0]));
     }
 
     // ----- GETTERS / SETTERS -----
+
+    public boolean hasParticleStyle(String styleName) {
+        for (ParticleStyleHolder holder : particleStyleList) {
+            if (holder.style.getName().equalsIgnoreCase(styleName)) return true;
+        }
+        return false;
+    }
 
     public int getParticleStyleCount() {
         if (particleStyleList != null) return particleStyleList.size();
@@ -118,8 +183,29 @@ public class ParticleGroup implements Cloneable {
         return name;
     }
 
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public List<ParticleStyleHolder> getParticleStyleList() {
+        return particleStyleList;
+    }
+
     public void addParticleStyle(ParticleStyle style) {
-        particleStyleList.add(style);
+        particleStyleList.add(new ParticleStyleHolder(style, new Vector(0, 0, 0)));
+    }
+
+    public int removeParticleStyle(String styleName) {
+        int count = 0;
+        for (int i = 0; i < particleStyleList.size(); i++) {
+            ParticleStyleHolder holder = particleStyleList.get(i);
+            if (holder.style.getName().equalsIgnoreCase(styleName)) {
+                particleStyleList.remove(i);
+                i--;
+                count++;
+            }
+        }
+        return count;
     }
 
     public void removeParticleStyle(int styleId) {
@@ -134,4 +220,58 @@ public class ParticleGroup implements Cloneable {
     public ParticleGroup clone() {
         return new ParticleGroup(this);
     }
+
+    // ----- HOLDER CLASS -----
+
+    public static class ParticleStyleHolder {
+
+        // ----- VARIABLES -----
+
+        public ParticleStyle style;
+        public Vector offset;
+        public Vector rotation = new Vector(0, 0, 0);
+        public Vector rotationSpeed = new Vector(0, 0, 0);
+
+        // ----- CONSTRUCTOR -----
+
+        public ParticleStyleHolder(ParticleStyle style, Vector offset) {
+            this.style = style;
+            this.offset = offset;
+        }
+
+        // ----- PARTICLE GETTING -----
+
+        public ParticleContainer[] getParticles(Location location) {
+            rotation.add(rotationSpeed);
+            return offsetParticles(rotateParticles(style.getParticles(location), location));
+        }
+
+        // TODO: Add option for not rotating style with group
+
+        public ParticleContainer[] getParticles(Location location1, Location location2) {
+            rotation.add(rotationSpeed);
+            return offsetParticles(rotateParticles(style.getParticles(location1, location2), location1));
+        }
+
+        // ----- PARTICLE OFFSETTING / ROTATING -----
+
+        private ParticleContainer[] offsetParticles(ParticleContainer[] particles) {
+            Vector rotatedOffset = VectorUtil.rotateVector(offset.clone(), rotation.getX(), rotation.getY(), rotation.getZ());
+            for (ParticleContainer p : particles) p.getLocation().add(rotatedOffset);
+            return particles;
+        }
+
+        private ParticleContainer[] rotateParticles(ParticleContainer[] particles, Location center) {
+            for (ParticleContainer particleContainer : particles) {
+                Vector current = particleContainer.getLocation().toVector();
+                Vector delta = new Vector(current.getX() - center.getX(), current.getY() - center.getY(), current.getZ() - center.getZ());
+                VectorUtil.rotateVector(delta, rotation.getX(), rotation.getY(), rotation.getZ());
+                Vector newPos = new Vector(delta.getX() + center.getX(), delta.getY() + center.getY(), delta.getZ() + center.getZ());
+                particleContainer.setLocationPosition(newPos);
+            }
+            return particles;
+        }
+
+    }
+
 }
