d648db9a89ddee1b33aa266950e1254a396f819a
Fusion1013 <william.reutmer@gmail.com>
2022-04-07 19:27:30 +0200

    Update extra settings

--- src/main/java/se/fusion1013/plugin/cobaltcore/particle/styles/ParticleStyleSphere.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/particle/styles/ParticleStyleSphere.java
@@ -1,5 +1,7 @@
 package se.fusion1013.plugin.cobaltcore.particle.styles;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import dev.jorel.commandapi.arguments.Argument;
 import dev.jorel.commandapi.arguments.BooleanArgument;
 import dev.jorel.commandapi.arguments.DoubleArgument;
@@ -24,6 +26,10 @@ public class ParticleStyleSphere extends ParticleStyle {
     private boolean inSphere;
 
     // ----- CONSTRUCTORS -----
+
+    public ParticleStyleSphere() {
+        super("sphere", "sphere_internal");
+    }
 
     public ParticleStyleSphere(String name, Particle particle, Vector offset, int count, double speed, Object extra) {
         super("sphere", name, particle, offset, count, speed, extra);
@@ -70,6 +76,22 @@ public class ParticleStyleSphere extends ParticleStyle {
         inSphere = (Boolean) args[2];
     }
 
+    @Override
+    public String getExtraSettings() {
+        JsonObject jo = new JsonObject();
+        jo.addProperty("density", density);
+        jo.addProperty("radius", radius);
+        jo.addProperty("in_sphere", inSphere);
+        return jo.toString();
+    }
+
+    @Override
+    public void setExtraSettings(String extra) {
+        JsonObject jsonObject = new Gson().fromJson(extra, JsonObject.class);
+        density = jsonObject.get("density").getAsInt();
+        radius = jsonObject.get("radius").getAsDouble();
+        inSphere = jsonObject.get("in_sphere").getAsBoolean();
+    }
 
     // ----- PARTICLE GETTING -----
 
@@ -108,6 +130,68 @@ public class ParticleStyleSphere extends ParticleStyle {
 
         return particles.toArray(new ParticleContainer[0]);
     }
+
+    // ----- BUILDER -----
+
+    public static class ParticleStyleSphereBuilder extends ParticleStyleBuilder<ParticleStyleSphere, ParticleStyleSphereBuilder> {
+
+        double targetRadius = 1;
+        double startRadius = 0;
+        boolean animateRadius = false;
+        int expandTime = 1;
+
+        int density = 1; // TODO: Change default value
+        boolean inSphere;
+
+        @Override
+        public ParticleStyleSphere build(){
+            obj.setRadius(targetRadius);
+            obj.setDensity(density);
+            obj.setInSphere(inSphere);
+            return super.build();
+        }
+
+        protected ParticleStyleSphere createObj() { return new ParticleStyleSphere(); }
+
+        protected ParticleStyleSphere.ParticleStyleSphereBuilder getThis() { return this; }
+
+        public ParticleStyleSphereBuilder animateRadius(double startRadius, int expandTime){
+            this.startRadius = startRadius;
+            this.expandTime = expandTime;
+            this.animateRadius = true;
+            return getThis();
+        }
+
+        public ParticleStyleSphereBuilder setRadius(double targetRadius){
+            this.targetRadius = targetRadius;
+            return getThis();
+        }
+
+        public ParticleStyleSphereBuilder setDensity(int density){
+            this.density = density;
+            return getThis();
+        }
+
+        public ParticleStyleSphereBuilder setInSphere(){
+            this.inSphere = true;
+            return getThis();
+        }
+    }
+
+    // ----- GETTERS / SETTERS -----
+
+    public void setDensity(int density) {
+        this.density = density;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void setInSphere(boolean inSphere) {
+        this.inSphere = inSphere;
+    }
+
 
     // ----- CLONE -----
 
