be6afb79053b78a6e94f4c523432a509ecccc3e0
Fusion1013 <william.reutmer@gmail.com>
2022-05-09 03:26:41 +0200

    Add dparticle command

--- src/main/java/se/fusion1013/plugin/cobaltcore/commands/particle/MainParticleCommand.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/commands/particle/MainParticleCommand.java
@@ -1,12 +1,26 @@
 package se.fusion1013.plugin.cobaltcore.commands.particle;
 
 import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.ParticleArgument;
+import org.bukkit.Particle;
 
 public class MainParticleCommand {
 
     // ----- REGISTER -----
 
     public static void register() {
+
+
+        new CommandAPICommand("dparticle")
+                .withPermission("cobalt.core.command.dparticle")
+                .withArguments(new ParticleArgument("particle"))
+                .executesPlayer(((sender, args) -> {
+                    Particle particle = (Particle) args[0];
+                    sender.getWorld().spawnParticle(particle, sender.getLocation(), 1);
+                }))
+                .register();
+
+
         new CommandAPICommand("cparticle")
                 .withPermission("cobalt.core.command.cparticle")
                 .withSubcommand(ParticleStyleCommand.createParticleStyleCommand())
