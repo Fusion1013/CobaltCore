956269af9eab754a36e332900591a9bde7b4577b
Fusion1013 <william.reutmer@gmail.com>
2022-06-07 20:34:08 +0200

    Update ParticleDebugEvent.java

--- src/main/java/se/fusion1013/plugin/cobaltcore/debug/ParticleDebugEvent.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/debug/ParticleDebugEvent.java
@@ -2,7 +2,19 @@ package se.fusion1013.plugin.cobaltcore.debug;
 
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParticleDebugEvent implements IDebugEvent {
+
+    List<String> description = new ArrayList<>();
+
+    public ParticleDebugEvent() {}
+
+    public ParticleDebugEvent addDescriptionLine(String line) {
+        this.description.add(line);
+        return this;
+    }
 
     @Override
     public String getName() {
@@ -10,5 +22,14 @@ public class ParticleDebugEvent implements IDebugEvent {
     }
 
     @Override
-    public void throwEvent(Player player) {}
+    public void throwEvent(Player player) {
+        for (String s : description) {
+            player.sendMessage(s);
+        }
+    }
+
+    @Override
+    public String[] getDescription() {
+        return description.toArray(new String[0]);
+    }
 }
