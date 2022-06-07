baf6a3215062326ddd775f75075a9e84db009b57
Fusion1013 <william.reutmer@gmail.com>
2022-06-07 20:34:44 +0200

    Add spawn parameters

--- src/main/java/se/fusion1013/plugin/cobaltcore/entity/ICustomEntity.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/entity/ICustomEntity.java
@@ -1,23 +1,26 @@
 package se.fusion1013.plugin.cobaltcore.entity;
 
 import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
 
 public interface ICustomEntity {
 
     /**
      * Forces the entity to spawn at the location.
      *
+     * @param spawnParameters parameters for the spawn.
      * @param location the location to spawn the entity.
      */
-    CustomEntity forceSpawn(Location location);
+    CustomEntity forceSpawn(Location location, ISpawnParameters spawnParameters);
 
     /**
      * Attempts to spawn the entity at the location, taking its natural spawn requirements into account.
      *
      * @param location the location to spawn the entity.
+     * @param spawnParameters parameters for the spawn.
      * @return null if the entity was not able to spawn.
      */
-    CustomEntity attemptNaturalSpawn(Location location);
+    CustomEntity attemptNaturalSpawn(Location location, ISpawnParameters spawnParameters);
 
     /**
      * Checks if the <code>CustomEntity</code> is alive.
@@ -31,5 +34,7 @@ public interface ICustomEntity {
     // ----- GETTERS / SETTERS -----
 
     String getInternalName();
+
+    EntityType getBaseEntityType();
 
 }
