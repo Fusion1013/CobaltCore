6188ad38549f3a00dbe589d33c2706e0c4f1386e
Fusion1013 <william.reutmer@gmail.com>
2022-06-07 20:33:40 +0200

    Pass internalVersion to getVersion

--- src/main/java/se/fusion1013/plugin/cobaltcore/database/system/ISystemDao.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/database/system/ISystemDao.java
@@ -2,7 +2,7 @@ package se.fusion1013.plugin.cobaltcore.database.system;
 
 public interface ISystemDao extends IDao {
 
-    int getVersion(String id);
+    int getVersion(String id, int internalVersion);
     void setVersion(String id, int version);
 
     @Override
