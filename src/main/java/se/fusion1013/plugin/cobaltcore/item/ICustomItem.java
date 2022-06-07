c670986922944786cfc433fb1512cb763963e26e
Fusion1013 <william.reutmer@gmail.com>
2022-06-07 20:36:37 +0200

    Add comments

--- src/main/java/se/fusion1013/plugin/cobaltcore/item/ICustomItem.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/item/ICustomItem.java
@@ -1,11 +1,31 @@
 package se.fusion1013.plugin.cobaltcore.item;
 
 import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.inventory.ItemStack;
 
 public interface ICustomItem {
+
+    // ----- GENERIC INFO GETTERS -----
+
     NamespacedKey getNamespacedKey();
     String getInternalName();
     ItemStack getItemStack();
     String[] getTags();
+
+    // ----- ITEM COMPARISON -----
+
+    /**
+     * Gets whether the <code>ItemStack</code> is an instance of the <code>ICustomItem</code>.
+     *
+     * @param item the <code>ItemStack</code> to check.
+     * @return whether the <code>ItemStack</code> is an instance of the <code>ICustomItem</code>.
+     */
+    boolean compareTo(ItemStack item);
+
+    // ----- ITEM ACTIVATOR EVENTS -----
+
+    void activatorTriggered(ItemActivator activator, Event event);
+
 }
