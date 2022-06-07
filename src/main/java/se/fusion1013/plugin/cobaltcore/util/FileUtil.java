23df027e4e11e34373eb968547377ef30e5d001c
Fusion1013 <william.reutmer@gmail.com>
2022-06-07 20:37:31 +0200

    Create saveJson

--- src/main/java/se/fusion1013/plugin/cobaltcore/util/FileUtil.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/util/FileUtil.java
@@ -1,13 +1,42 @@
 package se.fusion1013.plugin.cobaltcore.util;
 
 import org.bukkit.plugin.Plugin;
+import org.json.simple.JSONObject;
+import se.fusion1013.plugin.cobaltcore.CobaltCore;
+import se.fusion1013.plugin.cobaltcore.commands.system.CommandResult;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileUtil {
 
     /**
+     * Saves a <code>JSONObject</code> to the specified path.
+     *
+     * @param object the <code>JSONObject</code> to save.
+     * @param path the path to save it at.
+     * @return whether the file was created or not.
+     */
+    public static boolean saveJson(JSONObject object, String path) {
+        try {
+            File file = new File(path);
+            if (!file.exists()) file.getParentFile().mkdirs();
+
+            FileWriter fileWriter = new FileWriter(path);
+            fileWriter.write(object.toJSONString());
+            fileWriter.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
      * Gets a file from the specified path. If the file has not been created, create it from the plugin's resource folder if it exists.
+     *
      * @param plugin the plugin that is getting/creating the file.
      * @param filePath the path to the file.
      * @return the file, or null if file does not exist.
