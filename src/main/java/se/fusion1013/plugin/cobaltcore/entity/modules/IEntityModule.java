7881622cda9eb6cb8d3eb7d00de16f0ebd37d5bd
Fusion1013 <william.reutmer@gmail.com>
2022-06-07 20:35:54 +0200

    Implement spawn parameters

--- src/main/java/se/fusion1013/plugin/cobaltcore/entity/modules/IEntityModule.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/entity/modules/IEntityModule.java
@@ -1,9 +1,10 @@
 package se.fusion1013.plugin.cobaltcore.entity.modules;
 
 import se.fusion1013.plugin.cobaltcore.entity.CustomEntity;
+import se.fusion1013.plugin.cobaltcore.entity.ISpawnParameters;
 
 public interface IEntityModule {
 
-    void execute(CustomEntity customEntity);
+    void execute(CustomEntity customEntity, ISpawnParameters spawnParameters);
 
 }
