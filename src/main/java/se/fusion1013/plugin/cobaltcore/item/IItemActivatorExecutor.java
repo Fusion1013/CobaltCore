dc9c7804c4a26afc9f74cd017f05f3aa5ef193ba
Fusion1013 <william.reutmer@gmail.com>
2022-06-16 22:12:27 +0200

    Add slot to item activator execute

--- src/main/java/se/fusion1013/plugin/cobaltcore/item/IItemActivatorExecutor.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/item/IItemActivatorExecutor.java
@@ -2,9 +2,10 @@ package se.fusion1013.plugin.cobaltcore.item;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
+import org.bukkit.inventory.EquipmentSlot;
 
 public interface IItemActivatorExecutor {
 
-    void execute(ICustomItem item, Event event);
+    void execute(ICustomItem item, Event event, EquipmentSlot slot);
 
 }
