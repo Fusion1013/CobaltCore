c557f402fce2a1afb0022d8bf5867a557f78a887
Fusion1013 <william.reutmer@gmail.com>
2022-08-19 15:04:30 +0200

    Add getDataManager method

--- src/main/java/se/fusion1013/plugin/cobaltcore/database/system/Dao.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/database/system/Dao.java
@@ -5,6 +5,10 @@ import se.fusion1013.plugin.cobaltcore.CobaltCore;
 
 public abstract class Dao {
 
+    protected DataManager getDataManager() {
+        return CobaltCore.getInstance().getManager(CobaltCore.getInstance(), DataManager.class);
+    }
+
     protected void async(Runnable asyncCallback) {
         Bukkit.getScheduler().runTaskAsynchronously(CobaltCore.getInstance(), asyncCallback);
     }
