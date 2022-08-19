548e2fa38f1d69f8e988cb3e79e1d51fd02735cf
Fusion1013 <william.reutmer@gmail.com>
2022-08-19 15:04:07 +0200

    Move connection & ps close to try variables

--- src/main/java/se/fusion1013/plugin/cobaltcore/database/spawner/CustomSpawnerDaoSQLite.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/database/spawner/CustomSpawnerDaoSQLite.java
@@ -32,11 +32,14 @@ public class CustomSpawnerDaoSQLite extends Dao implements ICustomSpawnerDao {
             "`activation_range` REAL NOT NULL," +
             "`spawn_radius` INTEGER NOT NULL," +
             "`cooldown` INTEGER," +
+            "`delay_summon` INTEGER NOT NULL, " +
+            "`play_sound` TEXT NOT NULL," +
+            "`play_sound_delayed` TEXT NOT NULL," +
             "PRIMARY KEY (`custom_spawner_uuid`)" +
             ");";
 
     public static String SQLiteCreateCustomSpawnerView = "CREATE VIEW IF NOT EXISTS custom_spawners_view AS" +
-            " SELECT custom_spawners.custom_spawner_uuid, custom_spawners.entity, custom_spawners.spawner_type, custom_spawners.spawn_count, custom_spawners.activation_range, custom_spawners.spawn_radius, custom_spawners.cooldown, locations.world, locations.x_pos, locations.y_pos, locations.z_pos" +
+            " SELECT custom_spawners.custom_spawner_uuid, custom_spawners.entity, custom_spawners.spawner_type, custom_spawners.spawn_count, custom_spawners.activation_range, custom_spawners.spawn_radius, custom_spawners.cooldown, custom_spawners.delay_summon, custom_spawners.play_sound, custom_spawners.play_sound_delayed, locations.world, locations.x_pos, locations.y_pos, locations.z_pos" +
             " FROM custom_spawners" +
             " INNER JOIN locations ON locations.uuid = custom_spawners.custom_spawner_uuid;";
 
@@ -44,10 +47,11 @@ public class CustomSpawnerDaoSQLite extends Dao implements ICustomSpawnerDao {
     public Map<Long, Map<Location, CustomSpawner>> getCustomSpawners() {
         Map<Long, Map<Location, CustomSpawner>> spawners = new HashMap<>();
 
-        try {
-            Connection conn = DataManager.getInstance().getSqliteDb().getSQLConnection();
-            PreparedStatement ps = conn.prepareStatement("SELECT * FROM custom_spawners_view");
-            ResultSet rs = ps.executeQuery();
+        try (
+                Connection conn = getDataManager().getSqliteDb().getSQLConnection();
+                PreparedStatement ps = conn.prepareStatement("SELECT * FROM custom_spawners_view");
+                ResultSet rs = ps.executeQuery()
+        ) {
 
             while (rs.next()) {
                 UUID uuid = UUID.fromString(rs.getString("custom_spawner_uuid"));
@@ -73,6 +77,14 @@ public class CustomSpawnerDaoSQLite extends Dao implements ICustomSpawnerDao {
 
                 if (spawner == null) continue;
 
+                int delaySummon = rs.getInt("delay_summon");
+                String playSound = rs.getString("play_sound");
+                String playSoundDelayed = rs.getString("play_sound_delayed");
+
+                spawner.addSpawnDelay(delaySummon);
+                spawner.addSound(playSound);
+                spawner.addDelayedSound(playSoundDelayed);
+
                 Map<Location, CustomSpawner> spawnerMap = spawners.computeIfAbsent(chunk, k -> new HashMap<>());
                 spawnerMap.put(location, spawner);
                 spawners.put(location.getChunk().getChunkKey(), spawnerMap);
@@ -87,10 +99,12 @@ public class CustomSpawnerDaoSQLite extends Dao implements ICustomSpawnerDao {
 
     @Override
     public void saveCustomSpawners(Map<Long, Map<Location, CustomSpawner>> spawners) {
-        try {
-            Connection conn = DataManager.getInstance().getSqliteDb().getSQLConnection();
+        try (
+                Connection conn = DataManager.getInstance().getSqliteDb().getSQLConnection();
+                PreparedStatement ps = conn.prepareStatement("INSERT OR REPLACE INTO custom_spawners(custom_spawner_uuid, entity, spawner_type, spawn_count, activation_range, spawn_radius, cooldown, delay_summon, play_sound, play_sound_delayed) VALUES(?,?,?,?,?,?,?,?,?,?)");
+                PreparedStatement psLocation = conn.prepareStatement("INSERT OR REPLACE INTO locations(uuid, world, x_pos, y_pos, z_pos, yaw, pitch) VALUES(?, ?, ?, ?, ?, ?, ?)")
+        ) {
             conn.setAutoCommit(false);
-            PreparedStatement ps = conn.prepareStatement("INSERT OR REPLACE INTO custom_spawners(custom_spawner_uuid, entity, spawner_type, spawn_count, activation_range, spawn_radius, cooldown) VALUES(?,?,?,?,?,?,?)");
 
             for (Map<Location, CustomSpawner> spawnerMap : spawners.values()) {
                 for (CustomSpawner spawner : spawnerMap.values()) {
@@ -102,8 +116,19 @@ public class CustomSpawnerDaoSQLite extends Dao implements ICustomSpawnerDao {
                     int spawnRadius = spawner.getSpawnRadius();
                     int cooldown = spawner.getCooldown();
                     Location location = spawner.getLocation();
+                    int delaySummon = spawner.getDelaySummon();
+                    String playSound = spawner.getPlaySound();
+                    String playSoundDelayed = spawner.getPlaySoundDelayed();
 
-                    DataManager.getInstance().getDao(ILocationDao.class).insertLocation(uuid, location);
+                    // Insert location
+                    psLocation.setString(1, uuid.toString());
+                    psLocation.setString(2, location.getWorld().getName());
+                    psLocation.setDouble(3, location.getX());
+                    psLocation.setDouble(4, location.getY());
+                    psLocation.setDouble(5, location.getZ());
+                    psLocation.setDouble(6, location.getYaw());
+                    psLocation.setDouble(7, location.getPitch());
+                    psLocation.execute();
 
                     ps.setString(1, uuid.toString());
                     ps.setString(2, entity);
@@ -112,14 +137,15 @@ public class CustomSpawnerDaoSQLite extends Dao implements ICustomSpawnerDao {
                     ps.setDouble(5, activationRange);
                     ps.setInt(6, spawnRadius);
                     ps.setInt(7, cooldown);
+                    ps.setInt(8, delaySummon);
+                    ps.setString(9, playSound);
+                    ps.setString(10, playSoundDelayed);
 
                     ps.executeUpdate();
                 }
             }
 
             conn.commit();
-            conn.close();
-            ps.close();
 
         } catch (SQLException ex) {
             ex.printStackTrace();
@@ -129,12 +155,12 @@ public class CustomSpawnerDaoSQLite extends Dao implements ICustomSpawnerDao {
     @Override
     public void removeCustomSpawner(UUID uuid) {
         Bukkit.getScheduler().runTaskAsynchronously(CobaltCore.getInstance(), () -> {
-            try {
-                Connection conn = DataManager.getInstance().getSqliteDb().getSQLConnection();
-                PreparedStatement ps = conn.prepareStatement("DELETE FROM custom_spawners WHERE custom_spawner_uuid = ?");
+            try (
+                    Connection conn = getDataManager().getSqliteDb().getSQLConnection();
+                    PreparedStatement ps = conn.prepareStatement("DELETE FROM custom_spawners WHERE custom_spawner_uuid = ?")
+            ) {
                 ps.setString(1, uuid.toString());
                 ps.executeUpdate();
-                conn.close();
 
                 // Remove Location
                 DataManager.getInstance().getDao(ILocationDao.class).removeLocation(uuid);
