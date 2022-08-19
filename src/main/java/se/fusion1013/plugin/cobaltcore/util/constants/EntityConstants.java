6157003b1a7d3c6d641ddcf0199b826adf83ab96
Fusion1013 <william.reutmer@gmail.com>
2022-08-19 14:55:17 +0200

    Create EntityConstants.java

---
+++ src/main/java/se/fusion1013/plugin/cobaltcore/util/constants/EntityConstants.java
@@ -0,0 +1,10 @@
+package se.fusion1013.plugin.cobaltcore.util.constants;
+
+import org.bukkit.NamespacedKey;
+import se.fusion1013.plugin.cobaltcore.CobaltCore;
+
+public class EntityConstants {
+
+    public static final NamespacedKey IS_CUSTOM_ENTITY = new NamespacedKey(CobaltCore.getInstance(), "is_custom_entity");
+
+}
