c912e6bab8f09f4388a9b30a3341ac50e421fd55
Fusion1013 <william.reutmer@gmail.com>
2022-09-12 22:47:11 +0200

    Add random enchantment support

--- src/main/java/se/fusion1013/plugin/cobaltcore/item/loot/LootEntry.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/item/loot/LootEntry.java
@@ -1,14 +1,22 @@
 package se.fusion1013.plugin.cobaltcore.item.loot;
 
 import org.bukkit.inventory.ItemStack;
+import se.fusion1013.plugin.cobaltcore.util.ItemUtil;
 
 import java.util.Random;
 
 public class LootEntry { // TODO: Incorporate Loot Functions
 
+    // ----- VARIABLES -----
+
     ItemStack stack;
     int countMin;
     int countMax;
+
+    int enchantTier = -1;
+    int enchantAttempts = 0;
+
+    // ----- CONSTRUCTORS -----
 
     public LootEntry(ItemStack stack, int countMin, int countMax) {
         this.stack = stack;
@@ -16,16 +24,31 @@ public class LootEntry { // TODO: Incorporate Loot Functions
         this.countMax = countMax;
     }
 
+    // ----- BUILDER METHODS -----
+
+    public LootEntry addEnchant(int enchantTier, int enchantAttempts) {
+        this.enchantTier = enchantTier;
+        this.enchantAttempts = enchantAttempts;
+        return this;
+    }
+
+    // ----- GETTERS / SETTERS -----
+
     public ItemStack getStack(Random r) {
+        // Set stack amount
         if (countMin == countMax || countMax < countMin) {
             stack.setAmount(countMin);
         } else {
             stack.setAmount(r.nextInt(countMin, countMax));
         }
-        return stack.clone();
-    }
 
-    // ----- GETTERS / SETTERS -----
+        // Enchantment
+        if (enchantTier <= -1) return stack.clone();
+        else {
+            for (int i = 0; i < enchantAttempts; i++) ItemUtil.addWeightedEnchantment(stack.clone(), enchantTier);
+            return stack;
+        }
+    }
 
     public ItemStack stack() {
         return this.stack;
