460af0e9e0b8df4054ac0dfc9cdaa8a798dd3419
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2022-09-14 12:48:58 +0200

    Implement animator components

--- src/main/java/se/fusion1013/plugin/cobaltcore/animator/Animator.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/animator/Animator.java
@@ -1,4 +1,12 @@
 package se.fusion1013.plugin.cobaltcore.animator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Animator<T> {
+
+    List<IAnimatorComponent<T>> linkedAnimatorComponents = new ArrayList<>();
+
+
+
 }
