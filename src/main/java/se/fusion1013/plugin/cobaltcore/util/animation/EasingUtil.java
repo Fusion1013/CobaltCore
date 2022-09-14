12beefb36374b21d4ddb68cc3087c1d9d431505e
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2022-09-14 12:49:12 +0200

    Create EasingUtil.java

---
+++ src/main/java/se/fusion1013/plugin/cobaltcore/util/animation/EasingUtil.java
@@ -0,0 +1,52 @@
+package se.fusion1013.plugin.cobaltcore.util.animation;
+
+public class EasingUtil {
+
+    // ----- BACK -----
+
+    public static float  easeInBack(float t,float b , float c, float d) {
+        float s = 1.70158f;
+        return c*(t/=d)*t*((s+1)*t - s) + b;
+    }
+
+    public static float  easeInBack(float t,float b , float c, float d, float s) {
+        return c*(t/=d)*t*((s+1)*t - s) + b;
+    }
+
+    public static float  easeOutBack(float t,float b , float c, float d) {
+        float s = 1.70158f;
+        return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
+    }
+
+    public static float  easeOutBack(float t,float b , float c, float d, float s) {
+        return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
+    }
+
+    public static float  easeInOutBack(float t,float b , float c, float d) {
+        float s = 1.70158f;
+        if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525f))+1)*t - s)) + b;
+        return c/2*((t-=2)*t*(((s*=(1.525f))+1)*t + s) + 2) + b;
+    }
+
+    public static float  easeInOutBack(float t,float b , float c, float d, float s) {
+        if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525f))+1)*t - s)) + b;
+        return c/2*((t-=2)*t*(((s*=(1.525f))+1)*t + s) + 2) + b;
+    }
+
+    // ----- SINE -----
+
+    public static float  easeInSine(float t,float b , float c, float d) {
+        return -c * (float)Math.cos(t/d * (Math.PI/2)) + c + b;
+    }
+
+    public static float  easeOutSine(float t,float b , float c, float d) {
+        return c * (float)Math.sin(t/d * (Math.PI/2)) + b;
+    }
+
+    public static float  easeInOutSine(float t,float b , float c, float d) {
+        return -c/2 * ((float)Math.cos(Math.PI*t/d) - 1) + b;
+    }
+
+
+
+}
