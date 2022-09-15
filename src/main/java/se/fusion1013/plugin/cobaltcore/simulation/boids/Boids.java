da67c6f30dda6eaa431cceddde11502bbaacedb8
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2022-09-15 17:42:50 +0200

    Begin boid simulation implementation

--- src/main/java/se/fusion1013/plugin/cobaltcore/simulation/boids/Boids.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/simulation/boids/Boids.java
@@ -1,4 +1,19 @@
 package se.fusion1013.plugin.cobaltcore.simulation.boids;
 
+import se.fusion1013.plugin.cobaltcore.util.kdtree.KDTree;
+
+/**
+ * This implementation of the Boids algorithm uses the following simple rules;
+ * - Separation: Steer to avoid crowding local flockmates
+ * - Alignment: Steer toward the average heading of local flockmates
+ * - Cohesion: Steer to move towards the average position (center of mass) of local flockmates
+ */
 public class Boids {
+
+    KDTree kd; // KDTree is used to find nearby flockmates quickly
+    BoidEntity[] boidEntities;
+    int xRes;
+    int yRes;
+    // TODO: int zRes;
+
 }
