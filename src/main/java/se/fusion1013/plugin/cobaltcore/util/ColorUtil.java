5ce60b9812232a6daf157fca74f638418f6914df
Fusion1013 <william.reutmer@gmail.com>
2022-11-08 12:23:03 +0100

    Create fade method

--- src/main/java/se/fusion1013/plugin/cobaltcore/util/ColorUtil.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/util/ColorUtil.java
@@ -1,4 +1,15 @@
 package se.fusion1013.plugin.cobaltcore.util;
 
+import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.Color;
+
 public class ColorUtil {
+
+    public static TextColor fade(float ratio, TextColor far, TextColor close) {
+        int red = (int)Math.abs((ratio * far.red()) + ((1 - ratio) * close.red()));
+        int green = (int)Math.abs((ratio * far.green()) + ((1 - ratio) * close.green()));
+        int blue = (int)Math.abs((ratio * far.blue()) + ((1 - ratio) * close.blue()));
+
+        return TextColor.color(red, green, blue);
+    }
 }
