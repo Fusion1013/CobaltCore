479b06d7f8743c299606ebc79d47602376b25628
Fusion1013 <william.reutmer@gmail.com>
2022-11-08 12:11:10 +0100

    Create core action implementation

---
+++ src/main/java/se/fusion1013/plugin/cobaltcore/action/CoreActionFactory.java
@@ -0,0 +1,28 @@
+package se.fusion1013.plugin.cobaltcore.action;
+
+import se.fusion1013.plugin.cobaltcore.action.system.IAction;
+import se.fusion1013.plugin.cobaltcore.action.system.IActionFactory;
+import se.fusion1013.plugin.cobaltcore.action.system.IActionType;
+
+import java.util.Map;
+
+public class CoreActionFactory implements IActionFactory {
+
+    @Override
+    public IAction createAction(IActionType actionType, Map<?, ?> data) {
+        if (actionType == CoreActionType.VelocityAction) return new VelocityAction(data);
+        else if (actionType == CoreActionType.AudioAction) return new AudioAction(data);
+        else if (actionType == CoreActionType.ItemConsumeAction) return new ItemConsumeAction(data);
+        else if (actionType == CoreActionType.IsSneakingAction) return new IsSneakingAction();
+        return null;
+    }
+
+    @Override
+    public IAction createAction(String actionType, Map<?, ?> data) {
+        if (actionType.equalsIgnoreCase("velocity_action")) return new VelocityAction(data);
+        else if (actionType.equalsIgnoreCase("audio_action")) return new AudioAction(data);
+        else if (actionType.equalsIgnoreCase("item_consume_action")) return new ItemConsumeAction(data);
+        else if (actionType.equalsIgnoreCase("is_sneaking_action")) return new IsSneakingAction();
+        return null;
+    }
+}
---
+++ src/main/java/se/fusion1013/plugin/cobaltcore/action/CoreActionType.java
@@ -0,0 +1,14 @@
+package se.fusion1013.plugin.cobaltcore.action;
+
+import se.fusion1013.plugin.cobaltcore.action.system.IActionType;
+
+public enum CoreActionType implements IActionType {
+
+    // -- Action Actions
+    VelocityAction,
+    AudioAction,
+    ItemConsumeAction,
+
+    // -- Conditional Actions
+    IsSneakingAction
+}
