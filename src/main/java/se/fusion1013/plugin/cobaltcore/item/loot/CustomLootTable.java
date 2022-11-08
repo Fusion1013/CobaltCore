48307e12834e870c4923601873fd9b763a6e498d
Fusion1013 <william.reutmer@gmail.com>
2022-11-08 12:20:57 +0100

    Update item insert methods to randomly distribute items

--- src/main/java/se/fusion1013/plugin/cobaltcore/item/loot/CustomLootTable.java
+++ src/main/java/se/fusion1013/plugin/cobaltcore/item/loot/CustomLootTable.java
@@ -5,6 +5,8 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Container;
 import org.bukkit.inventory.ItemStack;
+import se.fusion1013.plugin.cobaltcore.item.CustomItemManager;
+import se.fusion1013.plugin.cobaltcore.item.ICustomItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,6 +46,21 @@ public class CustomLootTable {
 
     // ----- LOOT INSERTION -----
 
+    /**
+     * Drops loot from the <code>CustomLootTable</code> at the specified <code>Location</code>.
+     *
+     * @param location the <code>Location</code> to drop the loot at.
+     */
+    public void dropLoot(Location location) {
+        for (ItemStack stack : getLoot(Integer.MAX_VALUE)) location.getWorld().dropItemNaturally(location, stack);
+    }
+
+    /**
+     * Inserts loot into a loot container at the specified <code>Location</code>.
+     * The container must match one of the <code>CustomLootTable</code>'s <code>LootTarget</code>'s.
+     *
+     * @param location the <code>Location</code> to insert the loot at.
+     */
     public void insertLoot(Location location) {
         Random r = new Random();
 
@@ -64,21 +81,42 @@ public class CustomLootTable {
             if (!match) return;
 
             // Get all items to populate the inventory with
-            List<ItemStack> items = new ArrayList<>();
-
-            for (LootPool pool : pools) {
-                for (int i = 0; i < pool.rolls(); i++) {
-                    if (items.size() >= container.getInventory().getSize()) continue;
-                    items.add(pool.entries()[r.nextInt(pool.entries().length)].getStack(r)); // TODO: Add weights
-                }
-            }
+            List<ItemStack> items = getLoot(container.getInventory().getSize());
 
             // Fill the inventory
-            for (ItemStack item : items) {
-                int index = r.nextInt(container.getInventory().getSize());
-                container.getInventory().setItem(index, item);
-            }
+            insertItems(r, container, items);
         }
+    }
+
+    public static void insertItemsFromNames(Random r, Container container, List<String> itemStrings) {
+        for (String item : itemStrings) {
+            insertItem(CustomItemManager.getItemStack(item), container, r);
+        }
+    }
+
+    public static void insertItems(Random r, Container container, List<ItemStack> items) {
+        // Fill the inventory
+        for (ItemStack item : items) {
+            insertItem(item, container, r);
+        }
+    }
+
+    // Inserts an item without replacing current items, instead increasing item stack size
+    private static void insertItem(ItemStack item, Container container, Random r) {
+        int index = r.nextInt(container.getInventory().getSize());
+        ItemStack currentItem = container.getInventory().getItem(index);
+        ICustomItem customItem = CustomItemManager.getCustomItem(item);
+
+        if (currentItem == null) container.getInventory().setItem(index, item);
+        else if (customItem != null) if (customItem.compareTo(currentItem)) currentItem.setAmount(currentItem.getAmount() + item.getAmount());
+        else if (item.getType() == currentItem.getType()) currentItem.setAmount(currentItem.getAmount() + item.getAmount());
+        else insertItem(item, container, r);
+    }
+
+    // ----- GETTERS / SETTERS -----
+
+    public LootPool[] getPools() {
+        return pools;
     }
 
     // ----- TYPE ENUM -----
